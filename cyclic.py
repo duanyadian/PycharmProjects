388459e35ee83d5af25873b270b64b96d2e95ca8
duanyadian <785288076@qq.com>
2020-09-23 09:16:15 +0800

    if while语句

--- cyclic.py
+++ cyclic.py
@@ -26,3 +26,16 @@ while count<=100:
     sum=sum+count
     count=count+1
 print(sum,count)
+
+# ******************************
+
+java=86
+python=68
+
+if java >80 and python > 80:
+    print("优秀")
+else:
+    print("不优秀")
+
+if (java>80 and java<90) or (python>80 and python<90):
+    print("良好")
