5a533d6db6dbff6114d608e4ed2213c7a6661437
duanyadian <785288076@qq.com>
2020-09-25 13:51:23 +0800

    太阳花

--- sunflower.py
+++ sunflower.py
@@ -1,10 +1,10 @@
 from turtle import *
 color("red","yellow")
-begin_fill()
+begin_fill()           # 开始填充图形
 while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+    forward(200)       # 向前200
+    left(170)          # 向左170
+    if abs(pos()) < 1: # 查看画笔是否回到原点
+        break          # 如果回到，原点则跳出循环
+end_fill()             # 结束填充
+done()                 # 完成，页面不关闭
